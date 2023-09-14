766f34a2eb28a0051e583d8f04116ec1b7df51b4
Hamlet072797 <144698916+Hamlet072797@users.noreply.github.com>
2023-09-14 19:11:51 +0400

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,7 @@
-
+let config;
+export default  config={
+    base_url: 'https://marketplace.privateum.com/api/v1/',
+    // base_url: 'http://192.168.31.139:8001/api/v1/',
+    acceptLanguage: 'en',
+    acceptCurrency: 'USD'
+  };
