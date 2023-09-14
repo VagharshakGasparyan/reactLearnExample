38d9524b0d7882fe0887fa3ec5ddacda67e77016
Hamlet072797 <144698916+Hamlet072797@users.noreply.github.com>
2023-09-14 18:57:28 +0400

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,1 +1,50 @@
+import { Layout, Space } from "antd";
+import Buying_Format from "./slider/Buying_Format";
+import Brand from "./slider/Brand";
+import Country from "./slider/country";
+import axios from "axios";
+import("./Products.css");
+export default function Products() {
+  const { Header, Footer, Sider, Content } = Layout;
+
+
+
+  return (
+    <Space direction="vertical" className="vertical">
+      <Layout>
+        <Sider className="slider_style">
+          <div className="slider_block">
+            <div className="Buying_Format">
+              <h3>Buying Format</h3>
+              <br />
+              <Buying_Format />
+            </div>
+            <hr />
+
+            <div className="brand">
+              <h3>Brand</h3>
+              <br />
+             
+              <Brand />
+             
+            </div>
+            <hr />
+            <div className="country">
+            <h3>country</h3>
+             <br />
+             <Country/>
+
+            </div>
+            <hr />
+          </div>
+        </Sider>
+
+        <Layout>
+          <Content className="content_style">Content</Content>
+        </Layout>
+      </Layout>
+    </Space>
+  );
+}
+
 
